ab2059dfa2228fddb7a53baa7e6979e9b49cfbdf
shubini <shuaib2001.sa@gmail.com>
2024-01-17 21:05:00 +0000

    s2s6

--- FormalisingMathematics2024/Section02reals/Sheet6.lean
+++ FormalisingMathematics2024/Section02reals/Sheet6.lean
@@ -28,27 +28,58 @@ solving them.
 Good luck!
 -/
 /-- If `a(n)` tends to `t` then `37 * a(n)` tends to `37 * t`-/
+
+
 theorem tendsTo_thirtyseven_mul (a : ℕ → ℝ) (t : ℝ) (h : TendsTo a t) :
     TendsTo (fun n ↦ 37 * a n) (37 * t) := by
-  sorry
+  rw [tendsTo_def]
+  intro ε hε
+  obtain ⟨X, hX⟩ := h (ε / 37) (by linarith)
+  use X
+  intro n hn
+  specialize hX n hn
+  rw [← mul_sub]
+  rw [abs_mul]
+  rw [abs_of_nonneg] <;>
+  linarith
+
 
 /-- If `a(n)` tends to `t` and `c` is a positive constant then
 `c * a(n)` tends to `c * t`. -/
 theorem tendsTo_pos_const_mul {a : ℕ → ℝ} {t : ℝ} (h : TendsTo a t) {c : ℝ} (hc : 0 < c) :
     TendsTo (fun n ↦ c * a n) (c * t) := by
-  sorry
+  rw [tendsTo_def]
+  intro ε hε
+  obtain ⟨X, hX⟩ := h (ε / c) (by positivity)
+  use X
+  intro n hn
+  specialize hX n hn
+  rw [← mul_sub]
+  rw [abs_mul]
+  rw [abs_of_pos hc]
+  exact (lt_div_iff' hc).mp hX
 
 /-- If `a(n)` tends to `t` and `c` is a negative constant then
 `c * a(n)` tends to `c * t`. -/
 theorem tendsTo_neg_const_mul {a : ℕ → ℝ} {t : ℝ} (h : TendsTo a t) {c : ℝ} (hc : c < 0) :
     TendsTo (fun n ↦ c * a n) (c * t) := by
-  sorry
+  rw [tendsTo_def]
+  intro ε hε
+  have hc' : 0 < -c := neg_pos.mpr hc
+  obtain ⟨X, hX⟩ := h (ε / -c) (by positivity)
+  use X
+  intro n hn
+  specialize hX n hn
+  rw [← mul_sub]
+  rw [abs_mul]
+  rw [abs_of_neg hc]
+  exact (lt_div_iff' hc').mp hX
 
 /-- If `a(n)` tends to `t` and `c` is a constant then `c * a(n)` tends
 to `c * t`. -/
 theorem tendsTo_const_mul {a : ℕ → ℝ} {t : ℝ} (c : ℝ) (h : TendsTo a t) :
     TendsTo (fun n ↦ c * a n) (c * t) := by
-  sorry
+
 
 /-- If `a(n)` tends to `t` and `c` is a constant then `a(n) * c` tends
 to `t * c`. -/
