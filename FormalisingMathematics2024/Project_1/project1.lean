dc99e6b354e115c01f29ce35e5595da1956159c0
shubini <shuaib2001.sa@gmail.com>
2024-01-30 20:23:39 +0000

    update

--- FormalisingMathematics2024/Project_1/project1.lean
+++ FormalisingMathematics2024/Project_1/project1.lean
@@ -4,7 +4,7 @@ import LeanCopilot
 import Mathlib.LinearAlgebra.Isomorphisms
 
 open FiniteDimensional
-variable (k : Type) [Field k] (V : Type) [AddCommGroup V] [Module k V][FiniteDimensional k V]
+variable (k : Type) [Field k] (V : Type) [AddCommGroup V] [Module k V] [FiniteDimensional k V]
 
 
 lemma intersection_non_empty_if_sum_of_ranks_greater_than_dimension_of_ambient_space {A B : Subspace k V} (n m1 m2 : ℕ) (hV : FiniteDimensional.finrank k V = n) (hA : FiniteDimensional.finrank k A = m1) (hB : FiniteDimensional.finrank k B = m2) (hS: m1 + m2 > n) : A ⊓ B ≠ ⊥:= by
@@ -47,15 +47,28 @@ theorem finite_vector_spaces_are_isomorphic_if_they_have_the_same_dim {k : Type}
       Finset.mem_univ, ite_true, id_eq]
     exact Basis.sum_repr B v
   constructor
-  · refine Function.leftInverse_iff_comp.mpr h2
+  · exact Function.leftInverse_iff_comp.mpr h2
   · exact congrFun h1
   done
+
+example (m s : ℕ) : ¬m < s → s ≤ m := by
+  intro h
+  exact Nat.not_lt.mp h
 
 theorem finite_vector_spaces_have_the_same_dim_if_they_are_isomorphic {k : Type} [Field k] {V : Type} [AddCommGroup V] [Module k V][FiniteDimensional k V] {W : Type} [AddCommGroup W] [Module k W][FiniteDimensional k W] (h: V ≃ₗ[k] W): finrank k V = finrank k W := by
   let m := finrank k V
   let s := finrank k W
 
   by_contra h2
+  wlog hlt: m < s
+  . cases' Ne.lt_or_lt h2 with h3 h3
+    · contradiction
+    · specialize this k V
+      rw [Nat.not_lt] at hlt
+
+      exact lt_of_le_of_ne h3 (Ne.symm h2)
+
+
   have B : Basis (Fin m) k V := by
     exact FiniteDimensional.finBasisOfFinrankEq k V rfl
   have C: Basis (Fin s) k W := by
