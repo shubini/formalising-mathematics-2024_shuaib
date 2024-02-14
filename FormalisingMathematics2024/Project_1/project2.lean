5e00ec0f420f2f6b1320ebd21d98a5e50419a5c3
shubini <shuaib2001.sa@gmail.com>
2024-02-14 12:31:31 +0000

    project2 midterm q1 refactored and done

--- FormalisingMathematics2024/Project_1/project2.lean
+++ FormalisingMathematics2024/Project_1/project2.lean
@@ -14,7 +14,7 @@ Q7
 
 GROUPS AND RINGS PROBLEM SHEET 2 Q9
 
-GROUPS AND RINGS MIDTERM Q1 -- init
+GROUPS AND RINGS MIDTERM Q1 -- done
 
 CANT FIND BUT -- GROUP THEORY PROVE ANY INDEX 3 SUBGROUP OF AN ODD ORDER GROUP IS NORMAL
 
@@ -26,8 +26,6 @@ variable {G H} {x : G}
 
 variable {y z : G}
 
-lemma mul_inv_eq_inv_mul_inv (x y : G) : (x * y)⁻¹ = y⁻¹ * x⁻¹ := by group
-
 theorem centralizer.one_mem : (1 : G) ∈ {g : G | ∀ h ∈ H,  g * h * g⁻¹ = h} := by
   rintro h _
   group
@@ -37,7 +35,7 @@ theorem centralizer.inv_mem (hy : y ∈ {g : G | ∀ h ∈ H,  g * h * g⁻¹ = 
   intro h hh
   specialize hy h hh
   rw [hy.symm]
-  group at * -- evil floating point bit level hacking
+  group at *
   exact hy.symm
 
 theorem centralizer.mul_mem (hy : y ∈ {g : G | ∀ h ∈ H,  g * h * g⁻¹ = h})
@@ -46,13 +44,10 @@ theorem centralizer.mul_mem (hy : y ∈ {g : G | ∀ h ∈ H,  g * h * g⁻¹ = 
   intro h hh
   specialize hy h hh
   specialize hz h hh
-  rw [mul_inv_eq_inv_mul_inv]
-  rw [←mul_assoc]
+  rw [mul_inv_rev, ←mul_assoc]
   have hyz : y * z * h * z⁻¹ = y * (z * h * z⁻¹) := by
     group
-  rw [hyz]
-  rw [hz]
-  rw [hy]
+  rw [hyz, hz, hy]
 
 def centralizer (H : Subgroup G) : Subgroup G
     where
@@ -61,31 +56,23 @@ def centralizer (H : Subgroup G) : Subgroup G
   inv_mem' := centralizer.inv_mem
   mul_mem' := centralizer.mul_mem
 
-theorem cent_of_normal_is_normal (H : Subgroup G) [H_norm : H.Normal]: ∀ n, n ∈
+lemma cent_of_normal_is_normal (H : Subgroup G) [H_norm : H.Normal]: ∀ n, n ∈
     (centralizer H) → ∀ g : G, g * n * g⁻¹ ∈ (centralizer H):= by
-  rintro n hn g h hh
-  have H_norm_1 := Subgroup.Normal.conj_mem H_norm
-  have h2: ∀ h ∈ H, n * h * n⁻¹ = h := by -- del
-    exact fun h a => hn h a
-  specialize h2 h hh
-  specialize H_norm_1 h hh g
-  let h₂ := g⁻¹ * h* g
-  simp
-  rw [mul_assoc, mul_assoc]
-  have assoc_helper : (g⁻¹ * (h * (g * (n⁻¹ * g⁻¹)))) = (g⁻¹ * h * g) * (n⁻¹ * g⁻¹) := by group
-  rw [assoc_helper]
-  change g * n * (h₂ * (n⁻¹ * g⁻¹)) = h
-  have assoc_helper : g * n * (h₂ * (n⁻¹ * g⁻¹)) = g * (n * h₂ * n⁻¹) * g⁻¹ := by group
-  rw [assoc_helper]
-  have h2h : h₂ ∈ H := by
-    change (g⁻¹ * h * g) ∈ H
-    have a: g⁻¹ * h * g⁻¹⁻¹ ∈ H := by
-      exact Subgroup.Normal.conj_mem H_norm h hh g⁻¹
-    have b: (g⁻¹ * h * g) = (g⁻¹ * h * g⁻¹⁻¹) := by
-      group
-    rw [b]
-    exact a
-  specialize hn h₂ h2h
+  intro n hn g h hh
+  rw [show g * n * g⁻¹ * h * (g * n * g⁻¹)⁻¹ = g * (n * (g⁻¹ * h * g) * n⁻¹) * g⁻¹ by group]
+  have h2 : (g⁻¹ * h * g) ∈ H := by
+    rw [show (g⁻¹ * h * g) = (g⁻¹ * h * g⁻¹⁻¹) by group]
+    exact Subgroup.Normal.conj_mem H_norm h hh g⁻¹
+  specialize hn (g⁻¹ * h * g) h2
   rw [hn]
-  change g * (g⁻¹ * h * g) * g⁻¹ = h
   group
+
+def center := centralizer (⊤ : Subgroup G)
+theorem subofCentreNormal (H : Subgroup G) (hSub: H ≤ center) :  ∀ n, n ∈
+    H → ∀ g : G, g * n * g⁻¹ ∈ H:= by
+  intro h hh g
+  specialize hSub hh g (Subgroup.mem_top g)
+  rw [←eq_mul_of_mul_inv_eq hSub, mul_assoc, mul_inv_self g, mul_one]
+  exact hh
+
+-- PS1
