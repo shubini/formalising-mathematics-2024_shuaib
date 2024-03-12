cd6fd3396e14bca7c36a93307753345bc9acc1b3
shubini <shuaib2001.sa@gmail.com>
2024-03-12 14:49:15 +0000

    Init proj 3, first defs of alg varieties

---
+++ FormalisingMathematics2024/Project_1/project3.lean
@@ -0,0 +1,46 @@
+import Mathlib.Tactic
+import Mathlib.SetTheory.Cardinal.Finite
+open MvPolynomial
+
+
+def AffineVariety (f : MvPolynomial (Fin (n : ℕ)) ℂ) := {v : (Fin n) → ℂ | eval v f  = 0}
+
+--wip fix hypothesis
+theorem mul_poly_union_variety (f g: MvPolynomial (Fin n) ℂ)
+    {C₁ : AffineVariety f} {C₂ : AffineVariety g} : AffineVariety (f * g) =
+    {v : (Fin n) → ℂ | eval v f  = 0 ∨ eval v g  = 0} := by
+  ext v
+  rw [AffineVariety]
+  rw [Set.mem_setOf_eq, map_mul, mul_eq_zero, Set.mem_setOf_eq]
+  done
+
+/--Extension of affine varieties to multiple polynomials-/
+def AffineVariety' (σ : Fin (m : ℕ) → MvPolynomial (Fin (n : ℕ)) ℂ) :=
+    {v : (Fin n) → ℂ | ∀ p : Fin m, eval v (σ p) = 0}
+
+theorem mul_poly_intersect_variety (a b : Fin 2) (h : a ≠ b)
+    (σ : Fin 2 → MvPolynomial (Fin n) ℂ) :
+    AffineVariety' σ = {v : (Fin n) → ℂ | eval v (σ a)  = 0 ∧ eval v (σ b)  = 0} := by
+  ext v
+  rw [AffineVariety']
+  rw [Set.mem_setOf_eq, Set.mem_setOf_eq]
+  constructor
+  · intro h
+    constructor
+    · specialize h a
+      exact h
+    · specialize h b
+      exact h
+  · rintro ⟨ha, hb⟩ p
+    cases' eq_or_ne p a with hp hp
+    · rw [hp, ha]
+    · have h2 : Nat.card (Fin 2) = 2 := by
+        rw [Nat.card_eq_fintype_card]
+        rfl
+      have h2 := (Nat.card_eq_two_iff' a).mp h2
+      obtain ⟨b_, _,  hb2⟩ := h2
+      have hp := hb2 p hp
+      specialize hb2 b h.symm
+      rw [hp, ←hb2]
+      exact hb
+  done
